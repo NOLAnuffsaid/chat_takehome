69f0d4ef615e21dec4b67f1f97c4790d013b4358
Widner M <wjmillon@gmail.com>
2026-08-27 10:22:27 -0500

    Paginate chat message history

--- lib/chat_takehome/chat.ex
+++ lib/chat_takehome/chat.ex
@@ -9,6 +9,8 @@ defmodule ChatTakehome.Chat do
   alias ChatTakehome.Repo
   alias ChatTakehome.Users.User
 
+  @message_page_size 50
+
   @doc """
   Lists all chat messages in chronological order with their authors.
   """
@@ -17,6 +19,28 @@ defmodule ChatTakehome.Chat do
     |> order_by([message], asc: message.sent_at, asc: message.id)
     |> preload(:user)
     |> Repo.all()
+  end
+
+  @doc """
+  Lists the newest page of messages in chronological order.
+  """
+  def list_message_page(opts \\ []) do
+    page_size = page_limit(opts)
+
+    page_size
+    |> newest_messages_query()
+    |> fetch_message_page(page_size)
+  end
+
+  @doc """
+  Lists a page of messages older than the given message in chronological order.
+  """
+  def list_message_page_before(%Message{} = message, opts \\ []) do
+    page_size = page_limit(opts)
+
+    page_size
+    |> older_messages_query(message)
+    |> fetch_message_page(page_size)
   end
 
   @doc """
@@ -38,4 +62,43 @@ defmodule ChatTakehome.Chat do
 
   defp preload_message_user({:ok, message}), do: {:ok, Repo.preload(message, :user)}
   defp preload_message_user({:error, changeset}), do: {:error, changeset}
+
+  defp page_limit(opts) do
+    Keyword.get(opts, :limit, @message_page_size)
+  end
+
+  defp newest_messages_query(limit) do
+    Message
+    |> newest_first()
+    |> limit(^(limit + 1))
+  end
+
+  defp older_messages_query(limit, message) do
+    Message
+    |> where(
+      [candidate],
+      candidate.sent_at < ^message.sent_at or
+        (candidate.sent_at == ^message.sent_at and candidate.id < ^message.id)
+    )
+    |> newest_first()
+    |> limit(^(limit + 1))
+  end
+
+  defp newest_first(query) do
+    query
+    |> order_by([message], desc: message.sent_at, desc: message.id)
+    |> preload(:user)
+  end
+
+  defp fetch_message_page(query, page_size) do
+    messages = Repo.all(query)
+    has_more_messages? = length(messages) > page_size
+
+    messages =
+      messages
+      |> Enum.take(page_size)
+      |> Enum.reverse()
+
+    {messages, has_more_messages?}
+  end
 end
--- test/chat_takehome/chat_test.exs
+++ test/chat_takehome/chat_test.exs
@@ -3,6 +3,7 @@ defmodule ChatTakehome.ChatTest do
 
   alias ChatTakehome.Chat
   alias ChatTakehome.Chat.Message
+  alias ChatTakehome.Repo
 
   import ChatTakehome.UsersFixtures
 
@@ -29,6 +30,30 @@ defmodule ChatTakehome.ChatTest do
 
       assert Enum.any?(Chat.list_messages(), fn listed_message ->
                listed_message.id == message.id and listed_message.user == user
+             end)
+    end
+
+    test "paginates messages in chronological order" do
+      user = user_fixture()
+
+      messages =
+        for {body, offset} <- [{"First", 0}, {"Second", 1}, {"Third", 2}] do
+          {:ok, message} = Chat.create_message(user, %{body: body})
+
+          Repo.update!(
+            Ecto.Changeset.change(message,
+              sent_at: DateTime.add(~U[2099-01-01 00:00:00Z], offset)
+            )
+          )
+        end
+
+      {[second, third], true} = Chat.list_message_page(limit: 2)
+      assert [second.id, third.id] == [Enum.at(messages, 1).id, Enum.at(messages, 2).id]
+
+      {older_messages, _has_more_messages?} = Chat.list_message_page_before(second, limit: 2)
+
+      assert Enum.any?(older_messages, fn message ->
+               message.id == Enum.at(messages, 0).id
              end)
     end
   end
